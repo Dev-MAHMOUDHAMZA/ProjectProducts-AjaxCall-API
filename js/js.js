fcf6478ce6e5fd024b42a1afa9d973c61b4418c4
MAHMOUD HAMZA <90933334+Dev-MAHMOUDHAMZA@users.noreply.github.com>
2022-03-06 21:07:35 +0200

    Add Method Print

--- js/js.js
+++ js/js.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
     $('#tablPro').DataTable();
 });
+
+
+PrintTable = (el) => {
+
+    let body = document.body.innerHTML;
+
+    let x = document.getElementById(el).innerHTML;
+
+    document.body.innerHTML = x;
+
+    print();
+
+    document.body.innerHTML = body;
+
+
+
+};
